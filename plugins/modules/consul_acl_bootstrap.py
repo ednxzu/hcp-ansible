1fe5eeee85fc3ada3152d01d69ce17cc3c4f0b5f
Bertrand Lanson <bertrand.lanson@protonmail.com>
2024-01-27 21:11:36 +0100

    feat(consul): start on consul_acl_bootstrap module

--- plugins/modules/consul_acl_bootstrap.py
+++ plugins/modules/consul_acl_bootstrap.py
@@ -26,6 +26,17 @@ else:
     HAS_REQUESTS = True
 
 
+def bootstrap_acl(api_url):
+    # Your ACL bootstrap logic goes here
+    # You can use the 'requests' library to make HTTP requests to the Consul API
+    # For example:
+    # response = requests.post(api_url + '/v1/acl/bootstrap')
+    # Check the response and handle it accordingly
+
+    # For demonstration purposes, we assume the ACL bootstrap is successful
+    return True
+
+
 def run_module():
     module_args = dict(
         api_url=dict(type="str", required=True),
@@ -34,6 +45,33 @@ def run_module():
     result = dict(changed=False, state="")
 
     module = AnsibleModule(argument_spec=module_args, supports_check_mode=True)
+
+    api_url = module.params["api_url"]
+
+    try:
+        if not HAS_REQUESTS:
+            module.fail_json(
+                msg="Requests library is required but not installed. {}".format(
+                    REQUESTS_IMPORT_ERROR
+                )
+            )
+
+        # Perform ACL Bootstrap
+        acl_bootstrap_result = bootstrap_acl(api_url)
+
+        if acl_bootstrap_result:
+            result["changed"] = True
+            result["state"] = "ACL Bootstrap successful"
+        else:
+            result["changed"] = False
+            result["state"] = "ACL Bootstrap failed"
+
+        module.exit_json(**result)
+
+    except Exception as e:
+        module.fail_json(
+            msg="An error occurred during ACL Bootstrap: {}".format(str(e))
+        )
 
 
 def main():
