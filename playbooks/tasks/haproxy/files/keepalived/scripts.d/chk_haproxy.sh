267a23317f7cfaf699462ff7041d744fbe345e37
Bertrand Lanson <bertrand.lanson@protonmail.com>
2024-03-17 19:05:02 +0100

    feat(consul): start configuring and deploying agents

---
+++ playbooks/tasks/haproxy/files/keepalived/scripts.d/chk_haproxy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+function do_ping() {
+  wget -q -O /dev/null --server-response "http://127.0.0.1:9000/health" 2>&1 | awk '/^  HTTP/{print $2}'
+}
+
+ping_result=$(do_ping)
+echo "Ping result: $ping_result"
+
+if [ "$ping_result" = "200" ]; then
+  exit 0
+else
+  exit 1
+fi
